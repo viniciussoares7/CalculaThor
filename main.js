305cdbde1bd1199f6787edfb33e0458d41bd42f3
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-07-02 14:02:42 -0300

    working on javascript

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 function insert(num) {
-  var numero = document.getElementById('show').innerHTML
-  document.getElementById('show').innerHTML = numero + num
+  var number = document.querySelector('.show').innerHTML
+  document.querySelector('.show').innerHTML = number + num
+}
+
+function c() {
+  document.querySelector('.show').innerHTML = ''
+}
+
+function backspace() {
+  var result = document.querySelector('.show').innerHTML
+  document.querySelector('.show').innerHTML = result.substring(
+    0,
+    result.length - 1
+  )
+}
+function calc() {
+  var sum = document.querySelector('.show').innerHTML
+
+  if (sum) {
+    document.querySelector('.show').innerHTML = eval(sum)
+  }
 }
